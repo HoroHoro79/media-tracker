1fb4d68c4d6a250d4666c0bb41a9e3b97c7aacd8
Imanol <imanolperezpujante1@gmail.com>
2025-08-16 17:16:22 +0200

    Initial Spring Boot project setup

---
+++ media-tracker/src/test/java/com/imanol/media_tracker/MediaTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imanol.media_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediaTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
